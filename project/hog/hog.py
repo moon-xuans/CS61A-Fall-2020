25a981ffb65350636ca837a8e78ab3e9210e8aef
zhouxuan <moon_xuans@163.com>
2023-06-02 16:15:56 +0800

    Completed hog phase1

--- project/hog/hog.py
+++ project/hog/hog.py
@@ -23,6 +23,14 @@ def roll_dice(num_rolls, dice=six_sided):
     assert num_rolls > 0, 'Must roll at least once.'
     # BEGIN PROBLEM 1
     "*** YOUR CODE HERE ***"
+    dice_number = [0] * num_rolls
+    for i in range(num_rolls):
+        dice_number[i] = dice()
+    if 1 in dice_number:
+        return 1
+    else:
+        return sum(dice_number)
+
     # END PROBLEM 1
 
 
@@ -37,6 +45,7 @@ def free_bacon(score):
     # Trim pi to only (score + 1) digit(s)
     # BEGIN PROBLEM 2
     "*** YOUR CODE HERE ***"
+    pi = pi // pow(10, 100 - score)
     # END PROBLEM 2
 
     return pi % 10 + 3
@@ -57,6 +66,10 @@ def take_turn(num_rolls, opponent_score, dice=six_sided):
     assert opponent_score < 100, 'The game should be over.'
     # BEGIN PROBLEM 3
     "*** YOUR CODE HERE ***"
+    if num_rolls == 0:
+        return free_bacon(opponent_score)
+    else:
+        return roll_dice(num_rolls, dice)
     # END PROBLEM 3
 
 
@@ -79,7 +92,21 @@ def swine_align(player_score, opponent_score):
     """
     # BEGIN PROBLEM 4a
     "*** YOUR CODE HERE ***"
+    if player_score == 0 or opponent_score == 0:
+        return False
+    else: 
+        for i in range(10, player_score + 1):
+            if player_score % i == 0 and opponent_score % i == 0:
+                return True
+    return False
     # END PROBLEM 4a
+
+# def calc_gcd(player_score, opponent_score):
+#     ans = min(player_score, opponent_score)
+#     while ans >= 1:
+#         if player_score % ans == 0 and opponent_score % ans == 0:
+#             return ans
+#         ans -= 1
 
 
 def pig_pass(player_score, opponent_score):
@@ -101,6 +128,9 @@ def pig_pass(player_score, opponent_score):
     """
     # BEGIN PROBLEM 4b
     "*** YOUR CODE HERE ***"
+    if player_score < opponent_score and opponent_score - player_score < 3:
+        return True
+    return False
     # END PROBLEM 4b
 
 
@@ -140,6 +170,36 @@ def play(strategy0, strategy1, score0=0, score1=0, dice=six_sided,
     who = 0  # Who is about to take a turn, 0 (first) or 1 (second)
     # BEGIN PROBLEM 5
     "*** YOUR CODE HERE ***"
+    while True:
+        if who == 0:
+            who = other(who)
+            if strategy0(score0, score1) == 0:
+                score0 += free_bacon(score1)
+            else:
+                score0 += roll_dice(strategy0(score0, score1), dice)
+            while extra_turn(score0, score1):
+                say = say(score0, score1)
+                if score0 >= goal:
+                    break
+                score0 += take_turn(strategy0(score0, score1), score1, dice)
+            if score0 >= goal:
+                break
+            say = say(score0, score1)
+        if who == 1:
+            who = other(who)
+            if strategy1(score1, score0) == 0:
+                score1 += free_bacon(score0)
+            else:
+                score1 += roll_dice(strategy1(score1, score0), dice)
+            while extra_turn(score1, score0):
+                say = say(score0, score1)
+                if score1 >= goal:
+                    break
+                score1 += take_turn(strategy1(score1, score0), score0, dice)
+            if score1 >= goal:
+                break
+            say = say(score0, score1)
+    say = say(score0, score1)
     # END PROBLEM 5
     # (note that the indentation for the problem 6 prompt (***YOUR CODE HERE***) might be misleading)
     # BEGIN PROBLEM 6
